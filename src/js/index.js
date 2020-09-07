3cc166ae553c41831b91db6d1e283c3a3f82fc40
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-09-07 17:09:12 +0200

    added date function from moment library

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../scss/main.scss';
-import moment from 'moment'
+import moment from 'moment';
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -7,8 +7,8 @@ import moment from 'moment'
 
 /* place your code below */
 
-console.log('HELLO 🚀')
-console.log(moment().format('dddd MMMM YYYY, hh:mm:ss'));
+console.log('HELLO 🚀');
+console.log(moment().format('DD MMMM YYYY, hh:mm:ss'));
 
 const burgerButton = document.querySelector(".burger--js"); //tworze zmienna podłączoną do ikony hamburgera
 
@@ -20,3 +20,20 @@ burgerButton.addEventListener("click", () => {
   console.log(topMenu);
   topMenu.classList.toggle("navigation--mobile");
 });
+
+const whatDateButton = document.querySelector(".action--js");
+console.log(whatDateButton);
+
+const myAction = () => {
+    const newHeading = document.querySelector(".actionHeading--js");
+    newHeading.innerHTML = `Dzisiaj jest ${moment().format('dddd DD MMMM YYYY, [godzina] hh:mm')}`;
+}
+
+ /*  tworzę funkcję strzałkową myAction
+  pobieram zawartość klasy i przypisuję do zmiennej
+  wstrzykuję do zmiennej nową treść, zawartość jest automatycznie podmieniana w klasie actionHeading--j
+*/
+
+whatDateButton.addEventListener("click", myAction); //wywołuje funkcję myAction poprzez akcję click na elemencie myButton
+
+  //moment().format('DD MMMM YYYY, hh:mm:ss')

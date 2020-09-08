270715dd04b3c8afc6f5740737c25225808371fc
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-09-08 08:30:03 +0200

    added path to nauka.js

--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: {
     index: "./src/js/index.js",
     another: "./src/js/another.js",
+    nauka: "./src/js/nauka.js",
   },
   output: {
     filename: "[name].[hash:8].js",
@@ -97,7 +98,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/pages/nauka-js.html",
       inject: true,
-      chunks: ["nauka"],
+      chunks: ["index nauka"],
       filename: "nauka-js.html",
     }),
     new HtmlWebpackPlugin({

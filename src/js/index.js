52a69c87fa9ebbbbb64cf8451b8b8f8efb1c590a
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-09-11 10:09:38 +0200

    removed date button, added implementation of moment library to directly in heading

--- src/js/index.js
+++ src/js/index.js
@@ -21,19 +21,18 @@ burgerButton.addEventListener("click", () => {
   topMenu.classList.toggle("navigation--mobile");
 });
 
-const whatDateButton = document.querySelector(".action--js");
-console.log(whatDateButton);
 
-const myAction = () => {
-    const newHeading = document.querySelector(".actionHeading--js");
-    newHeading.innerHTML = `Dzisiaj jest ${moment().format('dddd DD MMMM YYYY, [godzina] hh:mm')}`;
-}
+const newHeading = document.querySelector(".actionHeading--js");
+
+newHeading.innerHTML = `Witam cię na mojej stronie o godzinie ${moment().format('hh:mm')}`;
+
+const whatDate = document.querySelector(".action--js");
+console.log(whatDate);
+
+whatDate.innerHTML = `Dzisiaj jest ${moment().format('dddd DD MMMM YYYY')}`;
+
 
  /*  tworzę funkcję strzałkową myAction
   pobieram zawartość klasy i przypisuję do zmiennej
   wstrzykuję do zmiennej nową treść, zawartość jest automatycznie podmieniana w klasie actionHeading--j
 */
-
-whatDateButton.addEventListener("click", myAction); //wywołuje funkcję myAction poprzez akcję click na elemencie myButton
-
-  //moment().format('DD MMMM YYYY, hh:mm:ss')

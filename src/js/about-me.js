df79596e16b2da3271a0c6a262df4d340bde3d58
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-12-15 13:22:16 +0100

    added feature tolist my repositories from stored JSON file got from github

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,1 +1,15 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
+
+// import stored json aquired from github
+import * as githubRepositoriesList from "../../myGithubRepository.json";
+
+// assign costant to element list from about-me page
+const repositoryListItem = document.querySelector(".github__repo--js");
+console.log(repositoryListItem);
+
+// for each element (repository) in json array
+for (let repository of githubRepositoriesList.default) {
+    // destructuring from object respository and extract properties
+  let { name, html_url, description } = repository;
+  repositoryListItem.innerHTML += `<li class="github__repository"><a href="${html_url}" title="${description}">${name}</a></li>`;
+}

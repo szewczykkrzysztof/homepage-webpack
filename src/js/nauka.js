b55bd910c57824bddedf38576b5d1f1ff37eaa0d
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-12-15 09:13:45 +0100

    repair burger menu on nauka-js page: removed burger function from nauka.js which disrupt burger.js functionality

--- src/js/nauka.js
+++ src/js/nauka.js
@@ -24,15 +24,4 @@ console.log(myName(firstName, age));
 
 console.log(myName("Tymon", 48));
 
-const burgerButton = document.querySelector(".burger--js"); //tworze zmienna podłączoną do ikony hamburgera
-
-/*przypisuję do przycisku akcję na klik
-tworzę zmienną zawierającą nawigację*/
-
-burgerButton.addEventListener("click", () => {
-  const topMenu = document.querySelector(".navigation--js");
-  console.log(topMenu);
-  topMenu.classList.toggle("navigation--mobile");
-});
-
 polishDate("2006-12-03");

6131328dea21833730c86b158b8a92b59b6ed504
Krzysztof Szewczyk <szewczykris@gmail.com>
2020-12-16 13:44:34 +0100

    Now respository list is fetch directly from Github API and sort by creation data

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,15 +1,18 @@
 import "../scss/main.scss";
 
-// import stored json aquired from github
-import * as githubRepositoriesList from "../../myGithubRepository.json";
-
 // assign costant to element list from about-me page
 const repositoryListItem = document.querySelector(".github__repo--js");
 console.log(repositoryListItem);
 
-// for each element (repository) in json array
-for (let repository of githubRepositoriesList.default) {
-    // destructuring from object respository and extract properties
-  let { name, html_url, description } = repository;
-  repositoryListItem.innerHTML += `<li class="github__repository"><a href="${html_url}" title="${description}">${name}</a></li>`;
-}
+// fetch my github respository list sorted from newest
+fetch("https://api.github.com/users/szewczykkrzysztof/repos?sort=created")
+  .then((resp) => resp.json())
+  .then((resp) => {
+    // for each element (repository) in json array
+    for (let repository of resp) {
+      // destructuring from object respository and extract properties
+      let { name, html_url, description } = repository;
+      repositoryListItem.innerHTML += `<li class="github__repository"><a href="${html_url}" title="${description}">${name}</a></li>`;
+    }
+  })
+  .catch(console.error("Błąd w pobieraniu danych z API Github"));
